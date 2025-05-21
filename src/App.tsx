e079d3c5b84fa77be69f33df6aa51e2cc01faa73
Denis Komogorcev <kdenru@gmail.com>
2025-05-21 18:14:09 +0700

    Добавил начальную настройку проекта с использованием Vite, React и TypeScript. Созданы основные файлы конфигурации, включая package.json, tsconfig и eslint.config.

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: 24, textAlign: 'center' }}>
+      Start page
+    </div>
+  )
+}
